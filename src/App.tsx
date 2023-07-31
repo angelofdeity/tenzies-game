959de842674ae4abb41a4cef18aba38384aa3bec
Nico <lawani.gk@gmail.com>
2023-07-31 13:04:28 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css"
+const App = () => {
+  return (
+    <main>
+      <div className="tenzies">
+        <div className="tenzies--main">
+          <h4>Tenzies</h4>
+          <p>Roll until all dice are the same.
+            Click each die to freeze it at its current value between rolls.
+          </p>
+        </div>
+
+      </div>
+    </main>
+  );
+}
+
+export default App;
